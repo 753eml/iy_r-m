a37351bd97d4b7776789191d0392494e4229db66
753eml <112660352+753eml@users.noreply.github.com>
2024-10-13 20:56:55 +0300

    Update iyrm.lua

--- iyrm.lua
+++ iyrm.lua
@@ -88,7 +88,7 @@ if not game:IsLoaded() then
 	notLoaded:Destroy()
 end
 
-currentVersion = '6.5.1'
+currentVersion = '6.5.2'
 Holder = Instance.new("Frame")
 Title = Instance.new("TextLabel")
 Dark = Instance.new("Frame")
