57b6f21640b5ad9c73d49c2b157738d966a24541
753eml <112660352+753eml@users.noreply.github.com>
2024-10-18 20:41:33 +0300

    Update iyrm.lua

--- iyrm.lua
+++ iyrm.lua
@@ -95,7 +95,7 @@ if not game:IsLoaded() then
 	notLoaded:Destroy()
 end
 
-currentVersion = '6.5.9'
+currentVersion = '6.6.0'
 Holder = Instance.new("Frame")
 Title = Instance.new("TextLabel")
 Dark = Instance.new("Frame")
@@ -10917,7 +10917,7 @@ addcmd('console',{},function(args, speaker)
 end)
 
 addcmd("unc",{"unctest","unccheckevn"},function(args, speaker)
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/main/UNCCheckEnv.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/753eml/UNC/refs/heads/main/unccheck.lua"))()
 	notify("Check console", "Press F9 or type /console in chat to see results")
 end)
 
